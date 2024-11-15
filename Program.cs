2093f4842dc0c55074482f209bb50aca7dcb69a2
Ramon-Mateus <ramonmateus00@gmail.com>
2024-11-15 03:41:29 -0300

    Retirado docker do .net e rodando apenas o banco por docker

--- Program.cs
+++ Program.cs
@@ -3,21 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlServerOptions =>
-        {
-            sqlServerOptions.EnableRetryOnFailure(
-                maxRetryCount: 5, // Número máximo de tentativas
-                maxRetryDelay: TimeSpan.FromSeconds(10), // Tempo máximo de espera entre tentativas
-                errorNumbersToAdd: null // Pode especificar erros específicos ou deixar null para todos
-            );
-        }
-    )
-);
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -31,7 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
