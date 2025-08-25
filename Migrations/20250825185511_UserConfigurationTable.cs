90da21726a4aa983bd7154ca1c642623c298785e
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2025-08-25 16:51:52 -0300

    Migrations de UserConfigurationTable

---
+++ Migrations/20250825185511_UserConfigurationTable.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ContaMente.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserConfigurationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cartoes_AspNetUsers_UserId",
+                table: "Cartoes");
+
+            migrationBuilder.CreateTable(
+                name: "UserConfigurations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ListagemPorFatura = table.Column<bool>(type: "boolean", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserConfigurations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserConfigurations_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserConfigurations_UserId",
+                table: "UserConfigurations",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cartoes_AspNetUsers_UserId",
+                table: "Cartoes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cartoes_AspNetUsers_UserId",
+                table: "Cartoes");
+
+            migrationBuilder.DropTable(
+                name: "UserConfigurations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cartoes_AspNetUsers_UserId",
+                table: "Cartoes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
